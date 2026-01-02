cef149c512bfa656cd42379eb5e0e6cadc1e55a9
616E74696661 <moriese@proton.me>
2026-01-02 15:45:09 +0100

    fixed bug "undef symbols for arm64"

--- src/position.hpp
+++ src/position.hpp
@@ -6,36 +6,29 @@
 
 struct Position {
 public:
-    int x;
-    int y;
+  int x;
+  int y;
 
-    Position(int xPos, int yPos) {
-        if (!isValid(xPos, yPos)) {
-            throw error_msg::INVALID_POSITION;
-        }
-         x = xPos, 
-         y = yPos;
+  Position(int xPos, int yPos) {
+    if (!isValid(xPos, yPos)) {
+      throw error_msg::INVALID_POSITION;
     }
+    x = xPos, y = yPos;
+  }
 
-    static bool isValid(int xPos, int yPos) {
-        for (const auto& p : validPositions) {
-            if (p.first == xPos && p.second == yPos) {
-                return true;
-            }
-        }
-        return false;
+  bool isValid(int xPos, int yPos) {
+    for (const auto &p : validPositions) {
+      if (p.first == xPos && p.second == yPos) {
+        return true;
+      }
     }
+    return false;
+  }
 
 private:
-    static constexpr std::array<std::pair<int, int>, 24> validPositions{{
-        {1,1}, {1,4}, {1,7},
-        {2,2}, {2,4}, {2,6},
-        {3,3}, {3,4}, {3,5},
-        {4,1}, {4,2}, {4,3},
-        {4,5}, {4,6}, {4,7},
-        {5,3}, {5,4}, {5,5},
-        {6,2}, {6,4}, {6,6},
-        {7,1}, {7,4}, {7,7}
-    }};
+  std::array<std::pair<int, int>, 24> validPositions{
+      {{1, 1}, {1, 4}, {1, 7}, {2, 2}, {2, 4}, {2, 6}, {3, 3}, {3, 4},
+       {3, 5}, {4, 1}, {4, 2}, {4, 3}, {4, 5}, {4, 6}, {4, 7}, {5, 3},
+       {5, 4}, {5, 5}, {6, 2}, {6, 4}, {6, 6}, {7, 1}, {7, 4}, {7, 7}}};
 };
 #endif // POSITION_HPP
