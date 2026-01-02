cb9c7b17fb157831ec2a8dcd870a71275f8594d7
LeonProgrammiert <leon.freudenmann@gmail.com>
2026-01-02 17:16:17 +0100

    Memory fix

--- src/main.cpp
+++ src/main.cpp
@@ -2,6 +2,18 @@
 #include "settings.cpp"
 #include "constants/error_messages.hpp"
 
+
+void handle_mills(Mills* m, Position* pos) {
+  std::vector<std::array<std::pair<int, int>, 3>> mills = pos->getPossibleMills();
+  for (const auto &mill : mills) {
+    //m->check_mill(mill);
+    std::cout<< mill[0].first << "," << mill[0].second << " - "
+             << mill[1].first << "," << mill[1].second << " - "
+             << mill[2].first << "," << mill[2].second << std::endl;  
+  }
+}
+
+
 int main() {
   std::cout << game_text::WELCOME << std::endl;
   
@@ -14,8 +26,20 @@ int main() {
   user* active_player = players.at(0);
 
   for (int i = 0; i < 10; i++) {
-    active_player->place_marker(&m);
+    // Select position
+    Position pos = active_player->place_marker();
+    // Place marker
+    m.player_set_stone(active_player->marker, &pos);
+    // Check for mills
+    handle_mills(&m, &pos);
     active_player = players.at((i + 1) % players.size()); // switch active player
     m.field_output();
   }
+
+  // Gameloop
+  bool run = true;
+  while (run) {
+
+  }
+
 }
--- src/position.hpp
+++ src/position.hpp
@@ -3,6 +3,32 @@
 
 #include "constants/error_messages.hpp"
 #include <array>
+#include <vector>
+
+static std::array<std::pair<int, int>, 24> validPositions{
+      {{1, 1}, {1, 4}, {1, 7}, {2, 2}, {2, 4}, {2, 6}, {3, 3}, {3, 4},
+       {3, 5}, {4, 1}, {4, 2}, {4, 3}, {4, 5}, {4, 6}, {4, 7}, {5, 3},
+       {5, 4}, {5, 5}, {6, 2}, {6, 4}, {6, 6}, {7, 1}, {7, 4}, {7, 7}}
+};
+
+static std::vector<std::array<std::pair<int, int>, 3>> validMills{
+      {{{1, 1}, {1, 4}, {1, 7}}},
+      {{{2, 2}, {2, 4}, {2, 6}}},
+      {{{3, 3}, {3, 4}, {3, 5}}},
+      {{{4, 1}, {4, 2}, {4, 3}}},
+      {{{4, 5}, {4, 6}, {4, 7}}},
+      {{{5, 3}, {5, 4}, {5, 5}}},
+      {{{6, 2}, {6, 4}, {6, 6}}},
+      {{{7, 1}, {7, 4}, {7, 7}}},
+      {{{1, 1}, {4, 1}, {7, 1}}},
+      {{{2, 2}, {4, 2}, {6, 2}}},
+      {{{3, 3}, {4, 3}, {5, 3}}},
+      {{{1, 4}, {2, 4}, {3, 4}}},
+      {{{5, 4}, {6, 4}, {7, 4}}},
+      {{{3, 5}, {4, 5}, {5, 5}}},
+      {{{2, 6}, {4, 6}, {6, 6}}},
+      {{{1, 7}, {4, 7}, {7, 7}}}
+};
 
 struct Position {
 public:
@@ -25,10 +51,17 @@ public:
     return false;
   }
 
-private:
-  std::array<std::pair<int, int>, 24> validPositions{
-      {{1, 1}, {1, 4}, {1, 7}, {2, 2}, {2, 4}, {2, 6}, {3, 3}, {3, 4},
-       {3, 5}, {4, 1}, {4, 2}, {4, 3}, {4, 5}, {4, 6}, {4, 7}, {5, 3},
-       {5, 4}, {5, 5}, {6, 2}, {6, 4}, {6, 6}, {7, 1}, {7, 4}, {7, 7}}};
+  std::vector<std::array<std::pair<int, int>, 3>> getPossibleMills() {
+    std::vector<std::array<std::pair<int, int>, 3>> mills;
+    for (const auto &mill : validMills) {
+      for (const auto &pos : mill) {
+        if (pos.first == x && pos.second == y) {
+          mills.push_back(mill);
+        }
+      }
+    }
+    return mills;
+  }  
 };
+
 #endif // POSITION_HPP
