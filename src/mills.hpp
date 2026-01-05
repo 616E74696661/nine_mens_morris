dc822b62e43b34c2ac574e4d20c36a42b322620c
616E74696661 <moriese@proton.me>
2026-01-05 22:54:06 +0100

    better output with animation

--- src/mills.hpp
+++ src/mills.hpp
@@ -4,6 +4,13 @@
 #include "constants/error_messages.hpp"
 #include "position.hpp"
 #include <iostream>
+#include <thread>
+
+#ifdef _WIN32
+const std::string OS = "windows";
+#else
+const std::string OS = "other"; // macos/linux
+#endif
 
 class Mills {
 public:
@@ -19,14 +26,28 @@ public:
   };
 
   void field_output() {
+
     for (int i = 0; i < Y; i++) {
       if (i % 2 == 0 && i != Y - 1)
         std::cout << 7 - (i / 2) << "  ";
       else
         std::cout << "   ";
       std::cout << field_template[i] << std::endl;
+      std::this_thread::sleep_for(std::chrono::milliseconds((long long)MS));
     }
   }
+
+  void clear_console() {
+    for (int i = 0; i < 30; i++) {
+      std::cout << "\n";
+      std::this_thread::sleep_for(std::chrono::milliseconds((long long)MS));
+    }
+    if (OS == "windows")
+      system("CLS");
+    else // for linux and macos
+      system("clear");
+  }
+
   void stone_set() {
     stones_set++;
   }
@@ -42,11 +63,14 @@ public:
   void next_phase() {
     if (current_phase < ENDGAME)
       current_phase = static_cast<GamePhase>(static_cast<int>(current_phase) + 1);
+    std::cout << "NEXT GAMEPHASE" << std::endl;
   }
 
   bool player_set_stone(char marker, Position* pos) {
     int pos_y = get_y_pos(pos->y);
     int pos_x = get_x_pos(pos->x);
+
+    clear_console();
 
     if (field_template[pos_y][pos_x] != T) {
       std::cout << error_msg::POSITION_OCCUPIED << std::endl;
@@ -55,6 +79,24 @@ public:
 
     field_template[pos_y][pos_x] = marker;
     std::cout << "successfully set :3" << std::endl;
+    stones_set++;
+    return true;
+  }
+
+  bool player_remove_stone(char marker, Position* pos) {
+    int pos_y = get_y_pos(pos->y);
+    int pos_x = get_x_pos(pos->x);
+
+    clear_console();
+
+    if (field_template[pos_y][pos_x] != T) {
+      std::cout << error_msg::POSITION_NOT_OCCUPIED << std::endl;
+      return false;
+    }
+
+    field_template[pos_y][pos_x] = marker;
+    std::cout << "successfully removed :3" << std::endl;
+    stones_set++;
     return true;
   }
 
@@ -69,14 +111,14 @@ public:
         counter++;
       }
     }
-
     return counter == 3;
   }
 
 private:
+  const int MS = 10;
   static const int X = 26;
   static const int Y = 15;
-  static const int STONES = 24;
+  static const int STONES = 18;
   static const char T = '#';
   GamePhase current_phase;
   int stones_set;
@@ -99,14 +141,26 @@ private:
       "",
       "1   2   3   4   5   6   7"};
 
+  char field_template2[Y][X] = {
+      "21---------22----------23",
+      "|           |           |",
+      "|  18------19------20   |",
+      "|   |       |       |   |",
+      "|   |  15--16--17   |   |",
+      "|   |   |       |   |   |",
+      "9--10--11      12--13--14",
+      "|   |   |       |   |   |",
+      "|   |   6---7---8   |   |",
+      "|   |       |       |   |",
+      "|   3-------4-------5   |",
+      "|           |           |",
+      "0-----------1-----------2",
+      "",
+      "1   2   3   4   5   6   7"};
+
   int get_y_pos(int pos_y) { return ((7 - (pos_y)) * 2); }
 
   // irrelevant when saving as [7][7] arr
   int get_x_pos(int pos_x) { return ((pos_x - 1) * 4); }
-
-  // irrelevant when saving as [7][7] arr
-  int calc_pos(int pos_y, int pos_x) {
-    return get_y_pos(pos_y) + get_x_pos(pos_x);
-  }
 };
 #endif
